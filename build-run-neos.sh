411695675ba462a1bc39a630e02d42140f19d663
Sebastian Kurfürst <sebastian@typo3.org>
2013-10-13 20:02:00 +0200

    [TASK] improve building of neos

--- build-run-neos.sh
+++ build-run-neos.sh
@@ -1,11 +1,11 @@
 ./build-base.sh
 
-if [ ! -f docker-neos-run/neos-latest.tar.gz ];
-then
-  cd neos-distribution
-  tar -czf ../docker-neos-run/neos-latest.tar.gz *
-  cd ..
-fi
+
+cd neos-distribution
+rm -Rf ../docker-neos-run/neos-latest.tar.gz
+tar -czf ../docker-neos-run/neos-latest.tar.gz *
+cd ..
+
 
 cd docker-neos-run
 sudo docker build -rm -t="sandstorm/neos-run" .
