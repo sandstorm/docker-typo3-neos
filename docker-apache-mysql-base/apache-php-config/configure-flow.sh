222b0cf161dd8ee97cb89056dd3fffe489bbf49c
Sebastian Kurfürst <sebastian@typo3.org>
2013-10-13 19:34:47 +0200

    [TASK] production context by default

--- docker-apache-mysql-base/apache-php-config/configure-flow.sh
+++ docker-apache-mysql-base/apache-php-config/configure-flow.sh
@@ -4,6 +4,8 @@ mysqld_safe &
 cd /var/www/
 rm -Rf Configuration/Development/Docker/Settings.yaml
 mkdir -p Configuration/Development/Docker/
+rm -Rf Configuration/Production/Docker/Settings.yaml
+mkdir -p Configuration/Production/Docker/
 
 echo "TYPO3:" >> Configuration/Development/Docker/Settings.yaml
 echo "  Flow:" >> Configuration/Development/Docker/Settings.yaml
@@ -16,3 +18,5 @@ echo "        password: ''" >> Configuration/Development/Docker/Settings.yaml
 echo "    utility:" >> Configuration/Development/Docker/Settings.yaml
 echo "      environment:" >> Configuration/Development/Docker/Settings.yaml
 echo "        temporaryDirectoryBase: '/tmp'" >> Configuration/Development/Docker/Settings.yaml
+
+cp Configuration/Development/Docker/Settings.yaml Configuration/Production/Docker/Settings.yaml
--- docker-apache-mysql-base/apache-php-config/start-apache-and-import-site.sh
+++ docker-apache-mysql-base/apache-php-config/start-apache-and-import-site.sh
@@ -3,5 +3,5 @@
 cd /var/www/
 FLOW_CONTEXT=Development/Docker ./flow doctrine:migrate
 FLOW_CONTEXT=Development/Docker ./flow site:import --packageKey TYPO3.NeosDemoTypo3Org
-
+FLOW_CONTEXT=Production/Docker ./flow cache:warmup
 /bin/bash
