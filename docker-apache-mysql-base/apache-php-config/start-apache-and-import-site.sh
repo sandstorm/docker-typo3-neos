00734bf6001e06a526b00999814bbc7f7b467ddf
Sebastian Kurfürst <sebastian@typo3.org>
2013-10-13 19:58:29 +0200

    [TASK] bugfixes

--- docker-apache-mysql-base/apache-php-config/start-apache-and-import-site.sh
+++ docker-apache-mysql-base/apache-php-config/start-apache-and-import-site.sh
@@ -4,4 +4,5 @@ cd /var/www/
 FLOW_CONTEXT=Development/Docker ./flow doctrine:migrate
 FLOW_CONTEXT=Development/Docker ./flow site:import --packageKey TYPO3.NeosDemoTypo3Org
 FLOW_CONTEXT=Production/Docker ./flow cache:warmup
+FLOW_CONTEXT=Production/Docker ./flow user:create admin password Firstname Lastname
 /bin/bash
