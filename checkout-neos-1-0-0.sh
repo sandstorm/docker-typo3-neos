769f19bbdfa1186003a504c80b69e7005659ed99
Sebastian Kurfürst <sebastian@typo3.org>
2013-12-12 03:21:50 +0100

    [TASK] add "checkout-neos-1.0.0" script"

---
+++ checkout-neos-1-0-0.sh
@@ -0,0 +1,4 @@
+wget -O neos-1.0.0.tar.gz "http://downloads.sourceforge.net/project/typo3flow/TYPO3%20Neos/1.0.0/Neos-1.0.0.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Ftypo3flow%2Ffiles%2FTYPO3%2520Neos%2F1.0.0%2F&ts=1386814755&use_mirror=surfnet"
+tar -xzvf neos-1.0.0.tar.gz
+mv Neos-1.0.0 neos-distribution
+rm -Rf neos-1.0.0.tar.gz
